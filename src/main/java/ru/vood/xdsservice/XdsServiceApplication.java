69bf6138afacc5ac362a79506f64968b14728921
Vood <balitsky_igor@list.ru>
2020-08-11 18:53:21 +0300

    initial

---
+++ src/main/java/ru/vood/xdsservice/XdsServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.vood.xdsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XdsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vood/xdsservice/XdsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vood.xdsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XdsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
